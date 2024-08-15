37370bade558bb181e960b2d82d8b7c6191e8a2d
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-16 02:19:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// const clicking  = document.getElementById('button');
+// clicking.addEventListener('click' , function(){
+//     console.log("clicked");
+// });
+
+// const input = document.getElementById('button');
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+// input.addEventListener('click', clickAlert);
 function addingEventListener() {
+    
+    const btn = document.getElementById('button');
+    button.addEventListener('click', function() {
+        console.log('Button clicked!');
+    });
 }
